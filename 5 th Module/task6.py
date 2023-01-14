ad225dab61967c91057c446e654b7db917457879
Abu Muhammad <122550790+AbuMuhammad98@users.noreply.github.com>
2023-01-14 23:43:16 +0500

    task6 da o'zgarish boldi kod yakuniga yetjkazib qo'yildi

--- 5 th Module/task6.py
+++ 5 th Module/task6.py
@@ -1,3 +1,5 @@
-son = float(input("sonni kiriting: "))
-if son > 0 or son < 0:
-  print(son)
+son = int(input("sonni kiriting: "))
+if (son >= 10 and son <= 99) or (son >= -99 and son <= -10):
+  print(son, 'Ikki xonali son')
+else:
+  print(son, 'ikki xonali son emas')
